dcf87210185536b19f878b25cb8c3333ee12def3
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2019-01-03 23:52:39 +0300

    c

--- destination.py
+++ destination.py
@@ -12,9 +12,10 @@ R1_TO_BROKER = '10.10.3.2'
 def get_from_r1():
     R1Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     R1Socket.bind((R1_TO_BROKER,3001))
+    count = 0
     while 1:
         data,addr = R1Socket.recvfrom(500)
-        print(data)
+        print("Num:", count, "len:", len(data))
 
 get_from_r1()
 
