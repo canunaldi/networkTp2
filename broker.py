d0ada0a560049c9006611adb9a7c3a61b22012e7
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2019-01-03 23:07:36 +0300

    c

--- broker.py
+++ broker.py
@@ -40,7 +40,7 @@ def send_r1():
     while 1:
         #lock.acquire()
         if len(message_list) > next_seqnum:
-            print("ML: ",len(message_list)) 
+            #print("ML: ",len(message_list)) 
             print("NSq:", next_seqnum)  
             R1Socket.sendto(message_list[next_seqnum],(R1_TO_BROKER_send,3001))
             next_seqnum +=1
