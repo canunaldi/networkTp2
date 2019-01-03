99894fe61862e1159ea39d90e5d2ab6ef0ed396e
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2019-01-03 22:44:55 +0300

    commit

--- destination.py
+++ destination.py
@@ -12,8 +12,9 @@ R1_TO_BROKER = '10.10.3.2'
 def get_from_r1():
     R1Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     R1Socket.bind((R1_TO_BROKER,3001))
-    data,addr = R1Socket.recvfrom(500)
-    print(data)
+    while 1:
+        data,addr = R1Socket.recvfrom(500)
+        print(data)
 
 get_from_r1()
 
