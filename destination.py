2b7e4537dac3fa92334131bfa5a3eb41fcc612ef
CAN DURAN UNALDI <e2036523@ceng.metu.edu.tr>
2019-01-04 05:01:01 +0300

    c

--- destination.py
+++ destination.py
@@ -42,7 +42,7 @@ def get_from_r1():
             finish()
             break
         #print("Num:", count, "len:", len(data))
-        print(data)
+        #print(data)
         index = int(data[500:])
         coming_messages[0][index] = data[:500]
         R1Ack.sendto(data[500:],(R1_TO_BROKER_send,3004))
@@ -57,7 +57,7 @@ def get_from_r2():
     while 1:
         data,addr = R2Socket.recvfrom(506)
         #print("Num:", count, "len:", len(data))
-        print(data)
+        #print(data)
         if "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" in data:
             finish()
             break
